c03a3d0ef06705026e3138c5b2c917fe432b05dd
VAN DAC PHONG TRUC <vanphongtruc1808@gmail.com>
2025-10-07 10:10:00 +0700

    v1

---
+++ main_code/predict_test.py
@@ -0,0 +1,18 @@
+import os
+from ultralytics import YOLO
+from PIL import Image
+
+#Load trained model YOLOv11 after epoched - choose the best.pt
+model = YOLO('D:/VSCode/DCLP/main_code/runs/detect/train/weights/best.pt')
+
+result = model('D:\VSCode\DCLP\dataset_train/test_image/3.jpg')
+
+#Print the result to screen
+for r in result:
+    print(r.boxes)
+    im_array = r.plot()
+    im = Image.fromarray(im_array[..., ::-1]) #RGB PIL image
+    im.show()
+    save_path = os.path.join('D:/VSCode/DCLP/dataset_train/result', 'result.jpg')
+    im.save(save_path)
+
---
+++ main_code/pretrain.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+#Delete the reply file train before epoched
+import shutil, os
+
+runs_dir = "runs/detect"
+train_dir = os.path.join(runs_dir, "train")
+
+if os.path.exists(train_dir):
+    shutil.rmtree(train_dir)
+    print(f"Deleted folder: {train_dir}")
+
+#Main code to train
+if __name__=="__main__":
+    # Load a COCO-pretrained YOLO11n model
+    model = YOLO("yolo11m.pt")
+
+    # Train the model on the pretrain example dataset for 10 epochs
+    results = model.train(
+        data="D:\VSCode\DCLP\dataset_train\my_dataset.yaml", 
+        epochs=30, 
+        imgsz=512, 
+        batch=2, 
+        workers=0, 
+        device = 0,
+        cache=False
+        )
